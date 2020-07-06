5070cd901b782273fa9a9d24dbf96b80e6417b5b
Rutujshahare <51992103+Rutujshahare@users.noreply.github.com>
2020-07-06 13:31:57 +0530

    expanded widget added

--- RutujIDcard/lib/main.dart
+++ RutujIDcard/lib/main.dart
@@ -12,18 +12,54 @@ void main() =>
         appBar:AppBar(
           title:Text('Expanded Widget'),
           centerTitle:true,
-          backgroundColor:Colors.green[300],
         ),
         body:Row(
+          mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Container(),
-          Container(),
-          Container(),
+        Expanded(
+                  child: Container(
+            child:Image.asset('assets/images/LogoDesign.jpg'),
+            ),
+        ),
+         
+  Expanded(
+            child: Container(
+            padding:EdgeInsets.all(40),
+            color:Colors.amber,
+           child:RaisedButton.icon(
+              onPressed:() {
+               Container(child: 
+               Text('the battery alert'),
+                  );
+                },
+                icon:Icon(Icons.battery_alert),
+                label:Text('batt'),
+
+              ),
+
+            ),
+          ),
+  Expanded(
+            child: Container(
+               padding:EdgeInsets.all(40),
+              color:Colors.purpleAccent[200],
+              child:Text('3'),
+            ),
+          ),
+  Expanded(
+            child: Container(
+              color:Colors.cyan,
+              padding:EdgeInsets.all(40),
+              child:Text('2'),
+            ),
+          ),
         ],
         ),
       );
     }
   }
+
+      
 
 
 
